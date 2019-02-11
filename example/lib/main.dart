37a601ffae840aaed46248ee64f5a5474d8f124d
Norbert <norbertkozsir@gmail.com>
2019-02-11 10:37:34 +0100

    added some fields to pubspec

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,26 +2,25 @@ import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:dynamic_theme/theme_switcher_widgets.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
 
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new DynamicTheme(
-      defaultBrightness: Brightness.light,
-      data: (brightness) => new ThemeData(
-        primarySwatch: Colors.indigo,
-        brightness: brightness,
-      ),
-      themedWidgetBuilder: (context, theme) {
-        return new MaterialApp(
-          title: 'Flutter Demo',
-          theme: theme,
-          home: new MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-      }
-    );
+    return DynamicTheme(
+        defaultBrightness: Brightness.light,
+        data: (brightness) => ThemeData(
+              primarySwatch: Colors.indigo,
+              brightness: brightness,
+            ),
+        themedWidgetBuilder: (context, theme) {
+          return MaterialApp(
+            title: 'Flutter Demo',
+            theme: theme,
+            home: MyHomePage(title: 'Flutter Demo Home Page'),
+          );
+        });
   }
 }
 
@@ -30,67 +29,69 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Easy Theme"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Easy Theme"),
       ),
-      body: new Center(
-        child: new Column(
+      body: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            new RaisedButton(onPressed: changeBrightness, child: new Text("Change brightness"),),
-            new RaisedButton(onPressed: changeColor, child: new Text("Change color"),),
+            RaisedButton(
+              onPressed: changeBrightness,
+              child: const Text("Change brightness"),
+            ),
+            RaisedButton(
+              onPressed: changeColor,
+              child: const Text("Change color"),
+            ),
           ],
         ),
       ),
-      floatingActionButton: new FloatingActionButton(
+      floatingActionButton: FloatingActionButton(
         onPressed: showChooser,
-        child: new Icon(Icons.add),
+        child: const Icon(Icons.add),
       ),
-      bottomNavigationBar: new BottomNavigationBar(
-          items: [
-            new BottomNavigationBarItem(
-                icon: new Icon(Icons.insert_drive_file),
-                title: new Text("Tab 1")
-            ),
-            new BottomNavigationBarItem(
-                icon: new Icon(Icons.show_chart),
-                title: new Text("Tab 2")
-            ),
-          ],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.insert_drive_file), title: Text("Tab 1")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart), title: Text("Tab 2")),
+        ],
       ),
     );
   }
 
-
   void showChooser() {
-    showDialog(context: context, builder: (context) {
-      return new BrightnessSwitcherDialog(
-        onSelectedTheme: (brightness) {
-          DynamicTheme.of(context).setBrightness(brightness);
-        },
-      );
-    });
+    showDialog<void>(
+        context: context,
+        builder: (context) {
+          return BrightnessSwitcherDialog(
+            onSelectedTheme: (brightness) {
+              DynamicTheme.of(context).setBrightness(brightness);
+            },
+          );
+        });
   }
 
-
   void changeBrightness() {
-    DynamicTheme.of(context).setBrightness(Theme.of(context).brightness == Brightness.dark? Brightness.light: Brightness.dark);
+    DynamicTheme.of(context).setBrightness(
+        Theme.of(context).brightness == Brightness.dark
+            ? Brightness.light
+            : Brightness.dark);
   }
 
   void changeColor() {
-    DynamicTheme.of(context).setThemeData(new ThemeData(
-        primaryColor: Theme.of(context).primaryColor == Colors.indigo? Colors.red: Colors.indigo
-    ));
+    DynamicTheme.of(context).setThemeData(ThemeData(
+        primaryColor: Theme.of(context).primaryColor == Colors.indigo
+            ? Colors.red
+            : Colors.indigo));
   }
 }
-
-
-
